482069a78b9e3f325459b56c87065687d876798b
nnamaka <nnamaka7@gmail.com>
2022-09-08 12:28:51 -0700

    close call

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/*
+ *main - start of progrm
+
+ *Description - This program just prints using "puts" function
+
+ *Return: Returns 0 after successful execution
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
