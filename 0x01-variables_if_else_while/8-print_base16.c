e4ed052218d0b2689e7403393d1cda4820b4a8af
nnamaka <nnamaka7@gmail.com>
2022-09-09 12:43:16 -0700

    squestion 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 48; a++)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+	}
+
+	putchar(10);
+
+	return (0);
+}
