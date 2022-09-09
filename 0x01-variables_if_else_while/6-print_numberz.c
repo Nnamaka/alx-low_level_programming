30c77f5565b623208e9bda098d476e2a944d6b67
nnamaka <nnamaka7@gmail.com>
2022-09-09 12:24:34 -0700

    set new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main program
+ * Return: Return 0 on success
+ */
+int main(void)
+{
+	int a = 48;
+
+	for (; a < 58; a++)
+		putchar(a);
+
+	putchar(a);
+	return (0);
+}
