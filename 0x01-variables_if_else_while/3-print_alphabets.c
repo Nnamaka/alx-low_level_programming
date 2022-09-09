47d8313c5745a08242afbf62ea3a8bc3b76e731a
nnamaka <nnamaka7@gmail.com>
2022-09-09 11:50:11 -0700

    save new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Code entry
+ * Return: Returns 0 after successfull completion
+ */
+int main(void)
+{
+	int a, b;
+
+	a, b = 97, 65;
+
+	for (int a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+
+	for (int a = 65; a < 91; a++)
+		putchar(a);
+
+	return (0);
+}
