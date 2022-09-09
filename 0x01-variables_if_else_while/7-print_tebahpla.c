d98b34a4cda102e21ee8447e0101c6b8ba69c885
nnamaka <nnamaka7@gmail.com>
2022-09-09 12:31:48 -0700

    no 7 exercise

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int a = 122;
+
+	for (; a > 96; a--)
+		putchar(a);
+
+	putchar(10);
+
+	return (0);
+}
