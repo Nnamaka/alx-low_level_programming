efc8aa4ecd2c751d8a7d0fb22d4b3503516b2b8f
nnamaka <nnamaka7@gmail.com>
2022-09-13 16:24:34 -0700

    show

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of a number
+ * @b : integer argument of function
+ * Return: returns absolute value of number in integer
+ */
+int _abs(int b)
+{
+	if (b >= 0)
+		return (b);
+	else
+		return (-b);
+}
