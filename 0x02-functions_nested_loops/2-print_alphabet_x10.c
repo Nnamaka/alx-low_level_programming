79083d6a29090bf8ec71fddafcfe529736ec0ea9
nnamaka <nnamaka7@gmail.com>
2022-09-13 15:40:14 -0700

    matter

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet a-z
+ * Return: returns nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+
+	_putchar('\n');
+}
