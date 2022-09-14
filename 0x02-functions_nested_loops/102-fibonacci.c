26f3fc893d458020c3feba85b59401cdcc769b0d
nnamaka <nnamaka7@gmail.com>
2022-09-13 19:10:50 -0700

    y commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: returns 1 for success
+ */
+int main(void)
+{
+	int i;
+	long n1, n2, next;
+
+	n1 = 1;
+	n2 = 2;
+
+	for (i  = 0; i < 50; i++)
+	{
+		if (i == 49)
+			printf("%lu", n1);
+		else
+		{
+			printf("%lu, ", n1);
+			next = n1 + n2;
+			n1 = n2;
+			n2 = next;
+		}
+	}
+
+	print("\n");
+	return (0);
+}
