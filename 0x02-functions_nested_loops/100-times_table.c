6d41897559ff55c16c5112760d6b02518e8f8bb3
nnamaka <nnamaka7@gmail.com>
2022-09-13 18:54:20 -0700

    saving

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,38 +6,41 @@
  */
 void print_times_table(int n)
 {
-	int a, b;
-	int times;
+	int a, b, times;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			_putchar(48);
-
-			for (b = 1; b <= n; b++)
+			for (b = 0; b <= n; b++)
 			{
-				times = a * b;
-				_putchar(44);
-				_putchar(32);
-
-				if (times <= 9)
+				z = a * b;
+				if (z > 99)
 				{
+					_putchar(',');
 					_putchar(32);
-					_putchar(32);
-					_putchar(times + 48);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				else if (times <= 99)
+				else if (z > 9)
 				{
+					_putchar(',');
 					_putchar(32);
-					_putchar((times / 10) + 48);
-					_putchar((times / 10) + 48);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
-					_putchar(((times / 100) % 10) + 48);
-					_putchar(((times / 10) % 10) + 48);
-					_putchar((times % 10) + 48);
+					if (b != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
 			_putchar('\n');
