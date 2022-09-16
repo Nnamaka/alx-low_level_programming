fff955004cf366791db03518e416dcc23c08b11c
nnamaka <nnamaka7@gmail.com>
2022-09-15 23:26:21 -0700

    selected

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test function to check for posiive or negative
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
