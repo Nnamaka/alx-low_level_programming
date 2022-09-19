a52e73f84394f69df343bb09a37ad2428a036985
nnamaka <nnamaka7@gmail.com>
2022-09-19 07:46:52 -0700

    git

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: char pointer argument
+ * Return: returns nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(s + i); i += 2)
+		_putchar(*(s + i));
+
+	_putchar('\n');
+}
