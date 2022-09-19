e6e66876afd99b2470781b5b251e0d402ebaa484
nnamaka <nnamaka7@gmail.com>
2022-09-19 09:10:00 -0700

    about

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - copy from source to destination
+ * @dest: char pointer argument of the destination string
+ * @src: char pointer argument of the source string
+ * Return: returns 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+		*(dest + i) = *(s + i);
+
+	return (dest);
+}
