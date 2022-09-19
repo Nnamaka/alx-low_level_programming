8067e875feb7e028633d451b57b0b4c6db7733db
nnamaka <nnamaka7@gmail.com>
2022-09-19 08:56:39 -0700

    for days

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - print a numbert of values of an integer array
  * @a: pointer integer argument
@@ -11,7 +12,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 		if (i + 1 == n)
-			printf("%i");
+			printf("%i", *(a + i));
 		else
 			printf("%i, ", *(a + i));
 
