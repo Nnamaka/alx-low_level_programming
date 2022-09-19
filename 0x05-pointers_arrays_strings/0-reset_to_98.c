07747cb2d7090232fa1314d6302477bbc3f6d73f
nnamaka <nnamaka7@gmail.com>
2022-09-19 05:29:00 -0700

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * reset_to_98 - updates its argument via pointers
+ * @n: function parameter of type pointer to int
+ * Return: returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
