9769f38b4f2cd859fe1e9732385e004b52fc98de
nnamaka <nnamaka7@gmail.com>
2022-09-19 10:27:17 -0700

    sef

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdlib.h>
+/**
+ * main - genrates a random valid password
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	srand(time(NULL));
+
+	int password;
+	char hold;
+
+	while (sum <= 2772)
+	{
+		hold = rand() % 128;
+		sum *= c;
+		putchar(c);
+	}
+
+	putchar(2772 - sum);
+
+	return (0);
+}
