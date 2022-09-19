b9be6945081d1ab62d56255ed591e9b7d4c126f9
nnamaka <nnamaka7@gmail.com>
2022-09-19 08:53:28 -0700

    newline

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - print a numbert of values of an integer array
+ * @a: pointer integer argument
+ * @n: parameter to indicate the number of values to print
+ * Return: returns nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (i + 1 == n)
+			printf("%i");
+		else
+			printf("%i, ", *(a + i));
+
+	printf("\n");
+}
