05f84eb409025a052549e465dfc45f01fff644fa
nnamaka <nnamaka7@gmail.com>
2022-09-19 08:10:19 -0700

    movie

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: char pointer argument
+ * Return: returns nothing
+ */
+void puts_half(char *str)
+{
+	int i, n, j;
+
+	for (i = 0; *(s + i); i++)
+		;
+
+	n = (i - 1) / 2;
+
+	for (j = 0; j < n; j++)
+		_putchar(*(str + j));
+
+	_putchar('\n');
+}
