b911552220790a4a7b77ddeb02c4569474ac1ee0
nnamaka <nnamaka7@gmail.com>
2022-09-21 02:17:54 -0700

    first leg

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ * _strcat - adds the src string to the dest string
+ * @dest: the pointer argument to the destination string
+ * @src: the pointer argument to the source string
+ * Return: returns a pointer to the destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+		;
+
+	dest[i] = ' ';
+	i++;
+
+	for (j = 0; src[j]; j++)
+		dest[i++] = src[j];
+
+	return (dest);
+}
