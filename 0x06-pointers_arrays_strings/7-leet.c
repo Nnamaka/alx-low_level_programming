512ab2297fb50b9065cb32fa482a7375ba3c1176
nnamaka <nnamaka7@gmail.com>
2022-09-21 11:53:35 -0700

    set it

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,14 +8,15 @@ char *leet(char *str)
 {
 	int i, j;
 
-	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', 'T'};
+	char leet[] = "oOlLeEaAtT";
+	char fig[] = "0011334477";
 
 	for (i = 0; str[i]; i++)
 	{
-		for (j = 0; j < 7; j++)
+		for (j = 0; leet[j]; j++)
 			if (str[i] == leet[j] ||
 					str[i] - 32 == leet[j])
-				str[i] = j + '0';
+				str[i] = fig[j];
 	}
 
 	return (str);
