7e0dc245004c94ac3c319ed4c26ae86ed3f22979
nnamaka <nnamaka7@gmail.com>
2022-09-28 06:53:09 -0700

    for stars

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,20 +8,20 @@ int _sqrt_recursion(int n)
 {
 	if (n <= 0)
 		return (-1);
-	return (sqrt((n / 2), n));
+	return (_sqrt((n / 2), n));
 }
 
 /**
- * sqrt - iterate incrementaly over a number to check for square root
+ * _sqrt - iterate incrementaly over a number to check for square root
  * @a: integer argument
  * @b: integer argument
  * Return: returns value of square root of @b
 */
-int sqrt(int a, int b)
+int _sqrt(int a, int b)
 {
 	if (a * a == b)
 		return (a);
 	if (a == 1)
 		return (-1);
-	return ((a - 1), b);
+	return (_sqrt((a - 1), b));
 }
