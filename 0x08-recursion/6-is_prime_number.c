6984a5734d4407706298cc4d9b03f3cc572e506a
nnamaka <nnamaka7@gmail.com>
2022-09-28 07:57:32 -0700

    chessing

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * check_p - check if its a prime number or not
+ * @a: integer argument
+ * @b: integer argument
+ * Return: returns an integer 1 on success else 0
+*/
+int check_p(int a, int b)
+{
+	if (b % a == 0 || a < 2)
+		return (0);
+	if (a > b / 2)
+		return (1);
+	return (check_p(a + 1, b));
+}
+
+/**
+ * is_prime_number - confirms if a number is prime or not
+ * @n: integer argument
+ * Return: returns an integer 0 if is not a prime else 1
+*/
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (check_p(2, n));
+}
