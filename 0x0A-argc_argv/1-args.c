06b1446b72f635ed79c79843af818614604da458
nnamaka <nnamaka7@gmail.com>
2022-09-30 15:09:29 -0700

    see

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print number of argument
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: returns 0 for success else a non-negative value
+*/
+int main (int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i", argc - 1);
+	printf("\n");
+
+	return (0);
+}
