fdd361b3e1e538298d4e2ccea56407f1184bcd3e
nnamaka <nnamaka7@gmail.com>
2022-09-30 15:14:01 -0700

    win

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array of arguments
  * Return: returns 0 for success else a non-negative value
 */
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%i", argc - 1);
 	printf("\n");
