eaf196fa65a52d602b748bf925e5b384fb539f9a
nnamaka <nnamaka7@gmail.com>
2022-10-06 17:45:01 -0700

    night push

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,19 +11,16 @@
  */
 int *array_range(int min, int max)
 {
-	int *ar;
-	int i;
+	int *p;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
-
-	ar = malloc(sizeof(*ar) * ((max - min) + 1));
-
-	if (ar == NULL)
+	size = (max - min) + 1;
+	p = malloc(size * sizeof(*p));
+	if (p == NULL)
 		return (NULL);
-
-	for (i = 0; min <= max; i++, min++)
-		ar[i] = min;
-
-	return (ar);
+	for (i = 0; i < size && min <= max; i++, min++)
+		*(p + i) = min;
+	return (p);
 }
