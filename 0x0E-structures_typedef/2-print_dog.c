747134f77270771c005fbaec1b35f7fb7513c1da
nnamaka <nnamaka7@gmail.com>
2022-10-10 16:18:35 -0700

    see fast

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,19 +9,10 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 
-		if (d->age == 0.0)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age ? d->age : "(nil)");
 
-		if (d->owner == NULL)
-			printf("Owner: (nil\n");
-		else
-			printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
