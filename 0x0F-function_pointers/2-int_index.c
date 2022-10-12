6b9ca1ecc160b85ccfb3c960236c8910f50e0c21
nnamaka <nnamaka7@gmail.com>
2022-10-12 15:48:46 -0700

    soft

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/** 
+ * int_index - searches for an integer
+ *  @array: An array of integers
+ *  @size: length of array
+ *  @cmp: pointer to callback function
+ * Return: retuns an index on match else -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || !array || !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
