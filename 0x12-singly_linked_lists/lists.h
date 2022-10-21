97ae31e0f96b1c16349fa4aae0bf1956be8b173b
nnamaka <nnamaka7@gmail.com>
2022-10-20 17:03:18 -0700

    some thing

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* #ifndef SINGLY_LINKED_LIST
+#endif /* #ifndef SINGLY_LINKED_LIST */
