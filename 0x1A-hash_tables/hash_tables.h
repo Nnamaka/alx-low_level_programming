81d4846270b72c10a72fce27c59bbea6d96704cb
nnamaka <nnamaka7@gmail.com>
2023-01-09 18:29:17 -0800

    see

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -83,7 +83,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-void has:h_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 shash_table_t *shash_table_create(unsigned long int size);
