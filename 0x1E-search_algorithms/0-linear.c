ef6b5b217922d4c6cd7e62eb2d2e88df7ef746cd
nnamaka <nnamaka7@gmail.com>
2023-04-14 15:30:35 -0700

    set

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,11 +8,15 @@
 * Return: return index of found value
 */
 int linear_search(int *array, size_t size, int value)
+
 {
+	int i;
+
 	if (array == NULL)
 		return (-1);
 
-	for (int i = 0; i < size; i++)
+
+	for (i = 0; i < (int)size; i++)
 	{
 		if (value == array[i])
 			return (i);
