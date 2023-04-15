cbfa539ca9968061ac780545a66696d61653e700
nnamaka <nnamaka7@gmail.com>
2023-04-15 15:42:50 -0700

    unfinished business

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+/**
+* binary_search - searches for a value in a sorted integer array 
+* using binary search algorithm.
+* @array: Pointer to the first element of an integer array
+* @size: size of the array
+* @value: Value to be searched for.
+*
+* Return: index of the element that is equal to value or return -1
+* if otherwise.
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+
